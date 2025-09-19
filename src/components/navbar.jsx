6c07f984d6cfd8f233d0c577855931867293e8b2
Reketino <bjornevensk8@gmail.com>
2025-09-19 12:56:27 +0200

    endret retning på navbar i mobil visning

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div className="w-full flex items-center justify-between p-4 bg-[url('/Sykkylven.jpg')]  bg-no-repeat bg-cover min-h-[300px] min-w-[600px]">
       <h1 className="text-black text-right">Fjellturer i Sykkylven </h1>
-      <ul className="flex-row flex gap-2">
+      <ul className="flex flex-col md:flex-row gap-2">
         <li>
           <Link href="/">Hjem</Link>
         </li>
