9e3005df4bb9c25eb93bc649457466ba77c2c0fa
Reketino <bjornevensk8@gmail.com>
2025-10-01 11:02:57 +0200

    emojimap oppdatert

--- src/utils/emojiMap.js
+++ src/utils/emojiMap.js
@@ -1,19 +1,36 @@
-
 const emojiMap = {
-    clearsky_day: { text: "Klarvær (dag) ", emoji: "☀️" },
-    clearsky_night: { text: "Klarvær (natt) ", emoji: "🌙" },
-    fair_day: { text: "Lett skyet (dag) ", emoji: "🌤️" },
-    fair_night: { text: "Lett skyet (natt) ", emoji: "🌙☁️" },
-    partlycloudy_day: { text: "Delvis skyet (dag) ", emoji: "⛅" },
-    partlycloudy_night: { text: "Delvis skyet (natt) ", emoji: "🌙☁️" },
+  clearsky_day: { text: "Klarvær (dag) ", emoji: "☀️" },
+  clearsky_night: { text: "Klarvær (natt) ", emoji: "🌙" },
+
+  fair_day: { text: "Lett skyet (dag) ", emoji: "🌤️" },
+  fair_night: { text: "Lett skyet (natt) ", emoji: "🌙☁️" },
+
+  partlycloudy_day: { text: "Delvis skyet (dag) ", emoji: "⛅" },
+  partlycloudy_night: { text: "Delvis skyet (natt) ", emoji: "🌙☁️" },
   cloudy: { text: "Skyet ", emoji: "☁️" },
+
+  lightrain: { text: "Lett regn", emoji: "🌦️" },
   rain: { text: "Regn ", emoji: "🌧️" },
   heavyrain: { text: "Kraftig regn ", emoji: "🌧️💦" },
+
+  lightsnow: { text: "Lett snø", emoji: "🌨️" },
   snow: { text: "Snø ", emoji: "❄️" },
+  heavysnow: { text: "Kraftig Snø", emoji: "❄️❄️" },
+  heavysnowshowersday: { text: "Kraftige Snøbyger", emoji: "🌨️" },
+
+  lightsleet: { text: "Lett sludd", emoji: "🌨️🌧️" },
+  sleet: { text: "Sludd", emoji: "🌨️🌧️" },
+  heavysleet: { text: "Kraftig sludd", emoji: "🌨️🌧️💦" },
+
+  lightsleetshowers_day: { text: "Lette sluddbyger (dag)", emoji: "🌨️🌧️☀️" },
+  lightsleetshowers_night: { text: "Lette sluddbyger (natt)", emoji: "🌨️🌧️🌙" },
+  sleetshowers_day: { text: "Sluddbyger (dag)", emoji: "🌨️🌧️☀️" },
+  sleetshowers_night: { text: "Sluddbyger (natt)", emoji: "🌨️🌧️🌙" },
+  heavysleetshowers_day: { text: "Kraftige sluddbyger (dag)", emoji: "🌨️🌧️" },
+  heavysleetshowers_night: { text: "Kraftige sluddbyger (natt)", emoji: "🌨️🌧️🌙" },
+
   fog: { text: "Tåke ", emoji: "🌫️" },
   thunderstorm: { text: "Tordenvær ", emoji: "⛈️" },
-   heavysnow: { text: "Kraftig Snø", emoji: "❄️❄️" },
-    heavysnowshowersday: { text: "Kraftige Snøbyger", emoji: "🌨️" },
 };
 
 export default emojiMap;
