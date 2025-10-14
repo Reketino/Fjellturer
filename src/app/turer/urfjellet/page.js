648ce0e509ec4cedd07fd5e0fbaa604cf4d864a1
Reketino <bjornevensk8@gmail.com>
2025-10-14 20:19:19 +0200

    forbedringer på urfjell.

--- src/app/turer/urfjellet/page.js
+++ src/app/turer/urfjellet/page.js
@@ -30,7 +30,7 @@ export default function UrfjelletPage() {
           Urfjellet
         </h1>
 
-        <div className="flex justify-center mt-4 mb-2">
+        <div className="flex justify-center">
           <Image
             src="/urfjellet.jpg"
             alt="Urfjellet"
@@ -39,15 +39,16 @@ export default function UrfjelletPage() {
             className="rounded-3xl object-cover"
           />
         </div>
-        <p className="font-serif italic mb-4 mt-2">
+        <p className=" text-center font-bold mb-2">
           Urfjellet sett i fra Koppen
         </p>
 
-        <p className="text-center font-serif italic mb-2 mt-4">
+        
+        <p className="text-center font-serif italic mt-2">
           Urfjellet på 1267, er enda en av de mange flotte & lengre turene fra
           Velledalen.
         </p>
-        <p className="text-center font-serif italic mb-4 mt-2">
+        <p className="text-center font-serif italic mb-4">
           På toppen vil man få en fantastisk utsikt over Hammarsettindene,
           Langeneset, ned mot Sykkylven & utover mot Ålesund.
         </p>
@@ -62,11 +63,11 @@ export default function UrfjelletPage() {
           kilometer={9.75}
           tidsbruk="4-6 timer"
         />
-        <p className="text-center mt-4">
-          Turen starter fra parkeringen, som man ser ligger opp her.
+        <p className="text-center mt-4 mb-2">
+          Turen starter ved parkeringen som ligger like over fylkesvei 60 i Velledalen, som man ser her⬇️.
         </p>
 
-        <div className="rounded-lg overflow-hidden shadow-lg flex flex-col items-center">
+        <div className="max-w-2xl w-full mx-auto rounded-lg overflow-hidden shadow-lg flex flex-col items-center">
           <iframe
             src="https://www.google.com/maps/embed?pb=!4v1759416354794!6m8!1m7!1sVB8tHFsEs5ySb0u_Z5tLfA!2m2!1d62.29194116728266!2d6.657225408158998!3f57.65601464103331!4f7.634504413804564!5f2.347694816175464"
             className="w-full h-64 sm:h-96"
@@ -75,19 +76,22 @@ export default function UrfjelletPage() {
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           />
-          <p className="font-serif italic text-center font-semibold mt-2">
+          <p className=" text-center font-bold mt-0.5">
             Parkeringen🅿️.
           </p>
         </div>
 
-        <p className="text-center mt-4">
-          Etter man har parkert går turen først til Koppen, som du finner
+        <p className="text-center mt-2">
+          Etter man har parkert går turen først til Koppen,
+        </p>
+        <p className="text-center mb-2">
+           som du finner
           beskrivelse på her⬇️.
         </p>
 
         <div className="mt-4 flex flex-col items-center mb-4">
           <h2 className="font-semibold text-center">
-            Her er ruten til Koppen:
+            Ruten til Koppen:
           </h2>
           <Koppen />
         </div>
@@ -105,8 +109,8 @@ export default function UrfjelletPage() {
             className="rounded-lg object-cover"
             unoptimized
           />
-          <p className="font-serif italic text-center font-semibold mb-2">
-            Her ser man Svadfjellet fra Koppen.
+          <p className="text-center font-bold mb-0.5">
+            Svadfjellet sett fra Koppen.
           </p>
         </div>
 
@@ -115,66 +119,73 @@ export default function UrfjelletPage() {
           opp til Urfjellet.
         </p>
 
-        <section className="rounded-lg overflow-hidden shadow-lg p-6 bg-black">
-          <p className="text-center mb-6">
+         <p className="text-center mb-2">
             Som man ser på bildet og videoen under ⬇️
           </p>
 
-          <div className="flex flex-col md:flex-row gap-6 items-stretch">
-            <div className="flex-1 flex flex-col items-center justify-between bg-gray-950 p-4 rounded-lg shadow">
+       
+            <div className="rounded-lg overflow-hidden shadow-lg flex-1">
               <Image
-                src="/urfjelletiltopp.gif"
+                src="/urfjelletopp.png"
                 alt="Urfjellet sett fra Svadfjellet"
                 width={500}
                 height={400}
-                className="rounded-lg object-cover w-full h-64 md:h-full"
-                unoptimized
+                className="rounded-lg shadow-md w-full h-96 object-contain"
               />
-              <p className="font-serif italic text-center mt-2 mb-2">
+              <p className=" text-center font-bold mt-0.5 mb-2">
                 ⚠️ Stien er mot slutten merket med varder, men masse ur!.
               </p>
             </div>
-            <div className="flex-1 flex flex-col items-center justify-between bg-gray-950 p-4 rounded-lg shadow">
+
+            <div className="rounded-lg overflow-hidden shadow-lg flex flex-col items-center mb-4">
               <iframe
-                className="w-full h-64 md:h-full rounded-lg shadow-lg"
+                width="360"
+                height="315"
                 src="https://www.youtube.com/embed/xnDaPwVOx-Y?si=6jUVaxlg1KNVaOtD"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
+                className="lg:w-2xl"
               />
-              <p className="font-serif italic text-center font-semibold mt-2 ">
+              <p className="font-serif italic text-center font-semibold mt-0.5 mb-4">
                 🎬 God film som viser ruten
               </p>
             </div>
-          </div>
-        </section>
+       
 
         
 
         <h2 className=" text-4xl font-bold text-center mt-4 mb-2">Toppen!🏔️</h2>
 
-        <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-4 items-center mb-2">
+        <section className="grid grid-cols-2 gap-4 mt-4">
+        <div className="rounded-lg overflow-hidden shadow-lg flex-1">
           <Image
-            src="/urfjelltopp.jpg"
+            src="/urfjelletutsikt.png"
             alt="Utsikt fra Toppen av Urfjellet"
             width={600}
             height={400}
-            className="rounded-lg object-cover w-full h-96"
+            className="rounded-lg shadow-md w-full h-96 object-cover"
           />
+           <p className="text-center font-bold mt-0.5">
+           Majestetisk utsikt over Hammarsettindene, Sykkylven & Ålesund👑.
+        </p>
+        </div>
+
+        <div className="rounded-lg overflow-hidden shadow-lg flex-1">
           <Image
             src="/urfjellet topp 2.jpg"
             alt="Utsikt fra Toppen av Urfjellet"
             width={600}
             height={400}
-            className="rounded-lg object-cover w-full h-96"
+            className="rounded-lg shadow-md w-full h-96 object-cover"
           />
+          <p className="text-center font-bold mt-0.5">
+            God oversikt over ryggen til Hammarsettindene🗻
+          </p>
         </div>
-        <p className="font-serif italic text-center font-semibold mt-2">
-          En majestetisk utsikt over Hammarsettindene, ut mot Sykkylven & videre
-          ut mot Ålesund👑.
-        </p>
+        </section>
 
         <h2 className="text-center text-xl font-extrabold mt-4 mb-2">
           Her er ruten opp til Urfjellet.
